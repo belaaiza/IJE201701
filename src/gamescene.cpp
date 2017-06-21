eea67eb76e8cc2a8df641ee09b1bf0c0cd2dbb61
Izabela Cardoso <cizabelacristina@gmail.com>
2017-06-21 13:38:20 -0300

    Adding victory condition

--- src/gamescene.cpp
+++ src/gamescene.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include "keyword.hpp"
 #include "player.hpp"
+#include "game.hpp"
 
 using namespace engine;
 
@@ -15,6 +16,20 @@ bool GameScene::init(){
 bool GameScene::update()
 {    
     Scene::update();
+    bool isWinner = true;
+    for (auto id_obj: m_objects)
+    {
+        auto obj = id_obj.second;
+        if ((dynamic_cast<Enemy*>(obj))){
+        	if(dynamic_cast<Enemy*>(obj)->life > 0){
+        		isWinner = false;
+        	}
+        }
+    }
+
+    if(isWinner){
+    	engine::Game::instance.change_scene("Victory");
+    }
 
     return true;
 }
